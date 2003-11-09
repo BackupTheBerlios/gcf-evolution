1d1a7d6b761f5ebffc8f4ec70ee7b56ef3ceb687
bodo <bodo>
2003-11-09 23:48:17 +0000

    Verhindere Start des Wurms, wenn nprocs nicht limmitiert wurde

--- evolution/src/worm.cpp
+++ evolution/src/worm.cpp
@@ -48,6 +48,7 @@ dieses Software-Paketes jederzeit zu ändern oder zu widerrufen.
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <unistd.h>
+#include "config.h"
 
 using namespace std;
 
